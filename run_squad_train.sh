203ddc0758306206cab111acde399c01f6125ba7
Suhas Gupta <gupta.suhas@gmail.com>
2019-07-17 18:53:31 -0700

    added run_squad fine tuning

---
+++ run_squad_train.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+BERT_LARGE_DIR='/home/suhasgupta/w266/gupta-hulburd-w266-final/BERT_LARGE_DIR'
+SQUAD_DIR='/home/suhasgupta/w266/gupta-hulburd-w266-final/'
+OUTPUT_DIR='/home/suhasgupta/w266/gupta-hulburd-w266-final/out'
+
+echo $BERT_LARGE_DIR
+echo $SQUAD_DIR
+echo $OUTPUT_DIR
+
+python3 bert/run_squad.py \
+  --vocab_file=$BERT_LARGE_DIR/vocab.txt \
+  --bert_config_file=$BERT_LARGE_DIR/bert_config.json \
+  --init_checkpoint=$BERT_LARGE_DIR/bert_model.ckpt \
+  --do_train=True \
+  --train_file=$SQUAD_DIR/train-v2.0.json \
+  --do_predict=True \
+  --predict_file=$SQUAD_DIR/dev-v2.0.json \
+  --train_batch_size=12 \
+  --learning_rate=3e-5 \
+  --num_train_epochs=2.0 \
+  --max_seq_length=32 \
+  --max_query_length=24 \
+  --doc_stride=128 \
+  --output_dir=$OUTPUT_DIR \
+  --use_tpu=False \
+  --version_2_with_negative=True
